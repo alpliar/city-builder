b8aaf1866c0090f0d7bdb22395d5feb5354dd503
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-02-02 02:40:47 +0100

    feature: handle leftIcon for button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+  import { Button } from "flowbite-svelte";
+  import type { ButtonProps } from "flowbite-svelte/buttons/Button.svelte";
   import { createEventDispatcher } from "svelte";
+  export let defaultClass: ButtonProps["defaultClass"] = undefined;
+  export let size: ButtonProps["size"] = "md";
+  $: iconClass = size === "md" ? "w-3 h-3" : "w-5 h-5";
 
   const dispatch = createEventDispatcher();
 
@@ -8,6 +13,11 @@
   };
 </script>
 
-<button on:click={handleClick}>
+<Button class={defaultClass} on:click={handleClick} {size}>
+  {#if $$slots.leftIcon}
+    <span class={iconClass}>
+      <slot name="leftIcon" />
+    </span>
+  {/if}
   <slot />
-</button>
+</Button>
